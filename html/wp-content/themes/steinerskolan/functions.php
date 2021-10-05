9d9606fcef114196156575bbd645608a585cf64d
San-Dan <sandradanielsson94@hotmail.com>
2021-10-05 23:52:10 +0200

    rm git cache for uploads, basic theme filesto be overwritten

--- html/wp-content/themes/steinerskolan/functions.php
+++ html/wp-content/themes/steinerskolan/functions.php
@@ -1,5 +1,17 @@
 <?php 
 
+declare(strict_types=1);
+
+add_action('after_setup_theme', function () {
+    add_theme_support('title-tag');
+    add_theme_support('menus');
+});
+
+add_action('wp_enqueue_scripts', function () {
+    wp_enqueue_style('style', get_stylesheet_uri());
+    wp_enqueue_style('header', get_template_directory_uri() . '/style.css');
+});
+
 function get_menu(string $location): Collection
     {
         $menu = [];
@@ -20,3 +32,5 @@ function get_menu(string $location): Collection
 
         return $menu;
     }
+
+ 
---
+++ html/wp-content/themes/steinerskolan/index.php
@@ -0,0 +1,9 @@
+<?php 
+
+get_header(); ?>
+
+<!-- The whole page -->
+
+<p></p>
+
+<?php get_footer(); ?>

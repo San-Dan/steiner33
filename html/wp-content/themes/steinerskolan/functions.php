642927f710e68474056f44215772fa502678e9ba
aseel <aseel@aseel.localdomain>
2021-10-06 20:46:58 +0200

    test commit

--- html/wp-content/themes/steinerskolan/functions.php
+++ html/wp-content/themes/steinerskolan/functions.php
@@ -2,35 +2,28 @@
 
 declare(strict_types=1);
 
-function get_menu(string $location): Collection
-{
-    $menu = [];
-    $items = wp_get_nav_menu_items($location) ?: [];
+// function get_menu(string $location): Collection
+// {
+//     $menu = [];
+//     $items = wp_get_nav_menu_items($location) ?: [];
 
-    foreach ($items as $item) {
-        $parentId = (int) $item->menu_item_parent;
+//     foreach ($items as $item) {
+//         $parentId = (int) $item->menu_item_parent;
 
-        if ($parentId === 0) {
-            $item->children = [];
-            $menu[$item->ID] = $item;
+//         if ($parentId === 0) {
+//             $item->children = [];
+//             $menu[$item->ID] = $item;
 
-            continue;
-        }
+//             continue;
+//         }
 
-        $menu[$parentId]->children[] = $item;
-    }
+//         $menu[$parentId]->children[] = $item;
+//     }
 
-    return $menu;
-}
+//     return $menu;
+// }
 
 add_action('wp_enqueue_scripts', function () {
     wp_enqueue_style('style', get_stylesheet_uri());
     wp_enqueue_style('header', get_template_directory_uri() . '/style.css');
 });
-
-// add_action('wp_enqueue_scripts', function () {
-//     wp_enqueue_script('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js', '', '', true);
-//     wp_enqueue_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css');
-//     wp_enqueue_style('style', get_theme_file_uri('assets/app.css'));
-//     wp_enqueue_script('script', get_theme_file_uri('assets/app.js'));
-// });

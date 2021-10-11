37ec056665b5f4ea55025fdb0b44a3a94e2cfab8
aseel <aseel@aseel.localdomain>
2021-10-11 19:22:13 +0200

    created anmalan page and styled it on mobile and desktop

--- html/wp-content/themes/steinerskolan/index.php
+++ html/wp-content/themes/steinerskolan/index.php
@@ -1,17 +1,17 @@
 <?php get_header(); ?>
 
 <div class="row">
-    <div class="col">
-        <?php if (have_posts()) : ?>
+    <!-- <div class="col"> -->
+    <!-- <?php if (have_posts()) : ?> -->
 
-            <?php while (have_posts()) : the_post(); ?>
+    <!-- <?php while (have_posts()) : the_post(); ?> -->
 
-                <?php the_content(); ?>
+    <?php the_content(); ?>
 
-            <?php endwhile; ?>
+    <!-- <?php endwhile; ?> -->
 
-        <?php endif; ?>
-    </div><!-- /col -->
-</div><!-- /row -->
-<div class="container">
-    <?php get_footer(); ?>
+    <!-- <?php endif; ?> -->
+    <!-- </div>/col -->
+    <!-- </div>/row -->
+    <div class="container">
+        <?php get_footer(); ?>

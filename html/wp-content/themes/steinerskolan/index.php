cde7114a6c8d5032ed65581895e15ea56a6cdb6e
aseel <aseel@aseel.localdomain>
2021-10-11 09:51:03 +0200

    added container to index.php

--- html/wp-content/themes/steinerskolan/index.php
+++ html/wp-content/themes/steinerskolan/index.php
@@ -1,17 +1,5 @@
 <?php get_header(); ?>
+<div class="container">
 
-<div class="row">
-    <div class="col">
-        <?php if (have_posts()) : ?>
 
-            <?php while (have_posts()) : the_post(); ?>
-
-                <?php the_content(); ?>
-
-            <?php endwhile; ?>
-
-        <?php endif; ?>
-    </div><!-- /col -->
-</div><!-- /row -->
-
-<?php get_footer(); ?>
+    <?php get_footer(); ?>
